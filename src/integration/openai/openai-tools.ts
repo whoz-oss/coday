27d8b0b64f9f18a033f737d8e2c749d602e94c14
vincent.audibert <vincent.audibert@whoz.com>
2024-07-12 21:52:50 +0200

    feat: expose a tool for Coday to ask questions to the user

--- src/integration/openai/openai-tools.ts
+++ src/integration/openai/openai-tools.ts
@@ -1,12 +1,11 @@
 import {RunnableToolFunction} from "openai/lib/RunnableFunction"
 import {Interactor} from "../../model/interactor"
 import {Beta} from "openai/resources"
-import AssistantTool = Beta.AssistantTool;
-import {CommandContext} from "../../model/command-context";
-import {AssistantToolFactory, Tool} from "../assistant-tool-factory";
+import {CommandContext} from "../../model/command-context"
+import {AssistantToolFactory, Tool} from "../assistant-tool-factory"
+import AssistantTool = Beta.AssistantTool
 
 export class OpenaiTools extends AssistantToolFactory {
-  
   constructor(interactor: Interactor) {
     super(interactor)
   }
@@ -17,21 +16,17 @@ export class OpenaiTools extends AssistantToolFactory {
   
   protected buildTools(context: CommandContext): Tool[] {
     const result: Tool[] = []
-    
     context.canSubTask(() => {
-      
       const subTask = ({subTasks}: { subTasks: { description: string }[] }) => {
         subTasks.forEach(
           subTask => this.interactor.displayText(`Sub-task received: ${subTask.description}`)
         )
         if (context.addSubTasks(...subTasks.map(subTask => subTask.description))) {
           return "sub-tasks received and queued for execution, will be runned after this current run."
-          
         }
         return "sub-tasks could not be queued, no more sub-tasking allowed for now."
       }
-      
-      const subTaskFunction: AssistantTool & RunnableToolFunction<{ subTasks: { description: string }[] }> = {
+      const subTaskTool: AssistantTool & RunnableToolFunction<{ subTasks: { description: string }[] }> = {
         type: "function",
         function: {
           name: "subTask",
@@ -51,15 +46,43 @@ export class OpenaiTools extends AssistantToolFactory {
                     }
                   }
                 }
-              },
+              }
             }
           },
           parse: JSON.parse,
           function: subTask
         }
       }
-      result.push(subTaskFunction)
+      result.push(subTaskTool)
     })
+    
+    if (!context.oneshot) {
+      const queryUser = ({message}: { message: string }) => {
+        const command = `add-query ${message}`
+        context.addCommands(command)
+        return "Query successfully queued, user will maybe answer later."
+      }
+      
+      const queryUserTool: AssistantTool & RunnableToolFunction<{ message: string }> = {
+        type: "function",
+        function: {
+          name: "queryUser",
+          description: "Queues asynchronously a query (question or request) for the user who may answer later, after this current run. IMPORTANT: Use this tool only when necessary, as it interrupts the flow of execution to seek user input.",
+          parameters: {
+            type: "object",
+            properties: {
+              message: {
+                type: "string",
+                description: "The query to be added to the queue for user answer."
+              }
+            }
+          },
+          parse: JSON.parse,
+          function: queryUser
+        }
+      }
+      result.push(queryUserTool)
+    }
     
     return result
   }
