418ef0de0ed872ca0ce7e6746547b4ae4b224d10
vincent.audibert <vincent.audibert@whoz.com>
2024-08-24 21:47:23 +0200

    comment: document failed attempts at leveraging file-map

--- src/function/generate-file-tree.ts
+++ src/function/generate-file-tree.ts
@@ -3,7 +3,7 @@ import {Interactor} from "../model"
 
 const maxBuffer = 50 * 1024 * 1024
 
-type FileTree = { [key: string]: FileTree | null } | null
+// type FileTree = { [key: string]: FileTree | null } | null
 
 export function generateFileTree(rootPath: string, interactor: Interactor, timeout: number = 10000, stringChunkSize: number = 200000): string[] {
   try {
@@ -13,7 +13,14 @@ export function generateFileTree(rootPath: string, interactor: Interactor, timeo
     const files = output.split("\n").filter(Boolean).sort()
     interactor.displayText(`... mapped ${files.length} files.`)
     
-    const fileTree: FileTree = {}
+    /*
+    Addition of pure text file trees with just indentation was not conclusive at all, folder perimeters were not detected.
+    Plain path printing yields better results but testing on a 20000 file repo overshot by a wide margin the allowed text length for initial instructions.
+    While possible to chunk, the interpretation done by the LLM seemed a bit off, it took WAAAYYY longer and token consumption is through the roof.
+    Future attempt should be made with a compressed json format of the filetree.
+    Also, it is difficult to warant no some out-of-buffer-size error occurs if ever increasing the file count.
+     */
+    // const fileTree: FileTree = {}
     
     // files.forEach(file => {
     //   const parts = file.split("/")
