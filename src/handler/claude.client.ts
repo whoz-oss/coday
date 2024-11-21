50242fa3ec8c464cfbd59066c651bf77e9147abc
vincent.audibert <vincent.audibert@whoz.com>
2024-11-21 18:36:36 +0100

    feat: clean up ai.client.ts and implementations

--- src/handler/claude.client.ts
+++ src/handler/claude.client.ts
@@ -1,22 +1,11 @@
-import {
-  Agent,
-  AiClient,
-  AiProvider,
-  AssistantDescription,
-  CommandContext,
-  DEFAULT_DESCRIPTION,
-  Interactor,
-  ModelSize
-} from "../model"
+import {Agent, AiClient, AiProvider, Interactor, ModelSize} from "../model"
 import Anthropic from "@anthropic-ai/sdk"
 import {MessageParam} from "@anthropic-ai/sdk/resources"
-import {Toolbox} from "../integration/toolbox"
-import {ToolCall, ToolResponse} from "../integration/tool-call"
 import {ToolSet} from "../integration/tool-set"
 import {CodayEvent, ErrorEvent, MessageEvent, ToolRequestEvent, ToolResponseEvent} from "../shared/coday-events"
 import {Observable, of, Subject} from "rxjs"
 import {AiThread} from "../ai-thread/ai-thread"
-import {RunStatus, ThreadMessage} from "../ai-thread/ai-thread.types"
+import {ThreadMessage} from "../ai-thread/ai-thread.types"
 
 const ClaudeModels = {
   [ModelSize.BIG]: {
@@ -33,12 +22,17 @@ export class ClaudeClient extends AiClient {
   aiProvider: AiProvider = "ANTHROPIC"
   multiAssistant = true
   private apiKey: string | undefined
-  private textAccumulator: string = ""
   
   private client: Anthropic | undefined
   
+  constructor(
+    private readonly interactor: Interactor,
+    private readonly apiKeyProvider: () => string | undefined,
+  ) {
+    super()
+  }
   
-  async answer2(agent: Agent, thread: AiThread): Promise<Observable<CodayEvent>> {
+  async run(agent: Agent, thread: AiThread): Promise<Observable<CodayEvent>> {
     if (!(await this.isReady())) return of()
     
     const outputSubject: Subject<CodayEvent> = new Subject()
@@ -95,15 +89,6 @@ export class ClaudeClient extends AiClient {
     }
   }
   
-  toolBox: Toolbox
-  
-  constructor(
-    private readonly interactor: Interactor,
-    private readonly apiKeyProvider: () => string | undefined,
-  ) {
-    super()
-    this.toolBox = new Toolbox(interactor)
-  }
   
   kill(): void {
     this.killed = true
@@ -121,119 +106,6 @@ export class ClaudeClient extends AiClient {
       return false
     }
     return true
-  }
-  
-  async addMessage(message: string, context: CommandContext): Promise<void> {
-    // Implement add message logic if applicable
-    this.interactor.error("NOT IMPLEMENTED !!!")
-    
-    /*
-    addMessage should be done at AiThread level instead of inside the client.
-    Client should only process a command with a given thread, not manage data
-     */
-  }
-  
-  async answer(name: string, command: string, context: CommandContext): Promise<string> {
-    this.textAccumulator = ""
-    if (!(await this.isReady())) {
-      return "Claude client not ready"
-    }
-    
-    if (!this.client) {
-      throw new Error("Anthropic client not initialized.")
-    }
-    
-    // init the thread data
-    if (!context.data.claudeData) {
-      context.data.claudeData = {
-        thread: new AiThread({id: `thread_${new Date().toISOString()}`, messages: []})
-      }
-    }
-    // Message accumulator equivalent to thread
-    const thread = context.data.claudeData.thread
-    
-    // Create new toolSet instance with current context tools
-    const toolSet = new ToolSet(this.toolBox.getTools(context))
-    
-    // add command to history
-    thread.addUserMessage("user", command)
-    
-    await this.processMessages(thread, DEFAULT_DESCRIPTION, toolSet, context)
-    
-    return this.textAccumulator
-  }
-  
-  async processMessages(
-    aiThread: AiThread,
-    assistant: AssistantDescription, // TODO: replace by AgentDefinition
-    toolSet: ToolSet,
-    context: CommandContext, // TODO: integrate relevant infos into AiThread ? Used just for project.description ?
-  ): Promise<void> {
-    const thinking = setInterval(() => this.interactor.thinking(), 3000)
-    
-    // define system instructions for the model (to be done each call)
-    const system = `${assistant.systemInstructions}\n\n
-        <project-context>
-${context.project.description}
-</project-context>`
-    
-    try {
-      const messages = this.toClaudeMessage(aiThread.getMessages())
-      const response = await this.client!.messages.create({
-        model: ClaudeModels[ModelSize.BIG].model,
-        messages,
-        system: system,
-        tools: this.getClaudeTools(toolSet),
-        max_tokens: 8192, // max powaaaa, let's ai-roast the planet !!!
-      })
-      
-      clearInterval(thinking)
-      
-      const text = response?.content?.filter(block => block.type === "text").map(block => block.text).join("\n")
-      this.textAccumulator += text
-      this.interactor.displayText(text, assistant.name)
-      
-      // push a compacted version of the assistant text response
-      if (text) {
-        aiThread.addAgentMessage(assistant.name, text)
-      }
-      
-      const toolUseBlocks = response?.content.filter(block => block.type === "tool_use")
-      const toolUses: ToolCall[] = toolUseBlocks.map(block => ({
-        id: block.id,
-        name: block.name,
-        args: JSON.stringify(block.input) // TODO: avoid stringification because just parsed after...
-      }))
-      
-      const toolResponses: ToolResponse[] = await Promise.all(
-        toolUses.map(async (call: ToolCall): Promise<ToolResponse> => {
-          const toolRequest = new ToolRequestEvent(call)
-          const response = await toolSet.runTool(toolRequest)
-          return {
-            id: call.id!,
-            name: call.name,
-            response
-          }
-        })
-      )
-      
-      if (toolUses?.length && toolResponses.length === toolUses.length) {
-        aiThread.addToolCalls(assistant.name, toolUses)
-        aiThread.addToolResponses("user", toolResponses)
-      }
-      
-      if (toolResponses.length && aiThread.runStatus === RunStatus.RUNNING) {
-        await this.processMessages(aiThread, assistant, toolSet, context)
-      }
-      
-    } catch (error: any) {
-      console.error(error)
-      throw new Error(`Error calling Claude API: ${error.message}`)
-    }
-  }
-  
-  reset(): void {
-    this.interactor.displayText("Conversation has been reset")
   }
   
   
