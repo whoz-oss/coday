5a20ee1e576b8d7ad5605770c187cb9501541864
Vincent Palita <vincent.palita@whoz.com>
2024-12-18 09:45:05 +0100

    feat(root): add nx release

--- jest.config.ts
+++ jest.config.ts
@@ -5,13 +5,14 @@ export default {
   roots: ['<rootDir>'],
   testMatch: ['**/?(*.)+(spec|test).+(ts|js)'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest'
   },
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  moduleFileExtensions: ['cjs', 'js', 'json', 'jsx', 'mjs', 'node', 'ts', 'tsx'],
+  extensionsToTreatAsEsm: ['.ts'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
   coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/', '/dist/'],
   // Useful for debugging
   verbose: true,
-};
+}
