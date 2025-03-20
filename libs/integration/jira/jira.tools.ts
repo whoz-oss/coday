a71356c4fa90e1b9ea3f7b6864b56b289f377a7c
vincent.audibert <vincent.audibert@whoz.com>
2025-03-20 11:39:18 +0100

    fix: JIRA tools init is cached
    And shared for same url, token and username, TTL 8h

--- libs/integration/jira/jira.tools.ts
+++ libs/integration/jira/jira.tools.ts
@@ -2,10 +2,10 @@ import { IntegrationService } from '../../service/integration.service'
 import { CommandContext, Interactor } from '../../model'
 import { AssistantToolFactory, CodayTool } from '../assistant-tool-factory'
 import { FunctionTool } from '../types'
-import { createJiraFieldMapping } from './jira-field-mapper'
 import { searchJiraIssuesWithAI } from './search-jira-issues'
 import { addJiraComment } from './add-jira-comment'
 import { retrieveJiraIssue } from './retrieve-jira-issue'
+import { jiraFieldMappingCache } from './jira-field-mapping-cache'
 
 export class JiraTools extends AssistantToolFactory {
   name = 'JIRA'
@@ -34,8 +34,8 @@ export class JiraTools extends AssistantToolFactory {
       return result
     }
 
-    // Generate custom field mapping during initialization
-    const { description: customFieldMappingDescription } = await createJiraFieldMapping(
+    // Get field mapping from cache service with 8-hour TTL
+    const { description: customFieldMappingDescription } = await jiraFieldMappingCache.getMappingForInstance(
       jiraBaseUrl,
       jiraApiToken,
       jiraUsername,
