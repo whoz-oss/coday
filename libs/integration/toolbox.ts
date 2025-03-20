870714322b75a7c7ab8c840e072e8eb215d12dff
vincent.audibert <vincent.audibert@whoz.com>
2025-03-20 08:57:03 +0100

    fix: JIRA tools selectables as other tools
    JIRA tools were async at build, now all tools are async and lifecycle is simplified

--- libs/integration/toolbox.ts
+++ libs/integration/toolbox.ts
@@ -1,4 +1,4 @@
-import { CommandContext, Interactor } from '../model'
+import { Interactor } from '../model'
 import { AiTools } from './ai/ai.tools'
 import { FileTools } from './file/file.tools'
 import { JiraTools } from './jira/jira.tools'
@@ -8,14 +8,12 @@ import { GitLabTools } from './gitlab/gitlab.tools'
 import { MemoryTools } from './memory.tools'
 import { AssistantToolFactory, CodayTool } from './assistant-tool-factory'
 import { ConfluenceTools } from './confluence/confluence.tools'
-import { AsyncAssistantToolFactory } from './async-assistant-tool-factory'
 import { CodayServices } from '../coday-services'
 import { AgentService } from '../agent'
 import { GetToolsInput } from './types'
 
 export class Toolbox {
   private toolFactories: AssistantToolFactory[]
-  private asyncToolFactories: AsyncAssistantToolFactory[]
   private tools: CodayTool[] = []
 
   constructor(interactor: Interactor, services: CodayServices, agentService: AgentService) {
@@ -27,23 +25,18 @@ export class Toolbox {
       new GitLabTools(interactor, services.integration),
       new MemoryTools(interactor, services.memory),
       new ConfluenceTools(interactor, services.integration),
+      new JiraTools(interactor, services.integration),
     ]
-    this.asyncToolFactories = [new JiraTools(interactor, services.integration)]
   }
 
-  getTools(input: GetToolsInput): CodayTool[] {
+  async getTools(input: GetToolsInput): Promise<CodayTool[]> {
     const { context, integrations, agentName } = input
-    this.tools = this.toolFactories
-      .filter((factory) => !integrations || integrations.has(factory.name))
-      .flatMap((factory) => factory.getTools(context, integrations?.get(factory.name) ?? [], agentName))
-    return this.tools
-  }
-
-  async getAsyncTools(context: CommandContext): Promise<CodayTool[]> {
-    const asyncTools = await Promise.all(
-      this.asyncToolFactories.map((asyncFactory) => asyncFactory.getAsyncTools(context))
-    )
-    this.tools = asyncTools.flat()
+    const filteredFactories = this.toolFactories.filter((factory) => !integrations || integrations.has(factory.name))
+    this.tools = (
+      await Promise.all(
+        filteredFactories.map((factory) => factory.getTools(context, integrations?.get(factory.name) ?? [], agentName))
+      )
+    ).flat()
     return this.tools
   }
 }
