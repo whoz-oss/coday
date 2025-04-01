f0127bd13af15f8f88004ddd349c1e908a73ef9a
vincent.audibert <vincent.audibert@whoz.com>
2025-04-01 14:59:35 +0200

    fix: increase write full file limit as models more powerful

--- libs/integration/file/write-file-by-path.ts
+++ libs/integration/file/write-file-by-path.ts
@@ -2,7 +2,7 @@ import { existsSync, mkdirSync, statSync, writeFileSync } from 'node:fs'
 import { Interactor } from '../../model/interactor'
 import path from 'path'
 
-const SIZE_THRESHOLD_KB = 6
+const SIZE_THRESHOLD_KB = 18
 const SIZE_THRESHOLD_BYTES = SIZE_THRESHOLD_KB * 1024
 
 type WriteFileByPathInput = {
