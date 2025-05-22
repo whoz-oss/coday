1712fc83a8cc03ede54d69bccb95aa014aaf6036
vincent.audibert <vincent.audibert@whoz.com>
2025-05-22 21:11:32 +0200

    Revert "build: testing if disabling enforce-module-boundaries works (ugly, will revert at some point)."
    This reverts commit 1773b350d62971766cfd8d11726cc254f41f6dd9.

--- apps/web/client/eslint.config.js
+++ apps/web/client/eslint.config.js
@@ -1,18 +1,4 @@
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import baseConfig from '../../../eslint.config.js'
 
-// Override the enforce-module-boundaries rule for this project
-const modifiedConfig = baseConfig.map(config => {
-  if (config.files && config.files.includes('**/*.ts') && config.rules && config.rules['@nx/enforce-module-boundaries']) {
-    return {
-      ...config,
-      rules: {
-        ...config.rules,
-        '@nx/enforce-module-boundaries': 'off' // Temporarily disable this rule
-      }
-    }
-  }
-  return config
-})
-
-export default modifiedConfig
+export default [...baseConfig]
