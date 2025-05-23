25b470e7fa9a71c7e4de111708c607330a1b049b
Vincent Palita <vincent.palita@whoz.com>
2025-05-23 09:19:22 +0200

    try to put lint back

--- apps/web/client/app.ts
+++ apps/web/client/app.ts
@@ -1,7 +1,7 @@
 import { ChatTextareaComponent } from './chat-textarea/chat-textarea.component'
 import { ChoiceSelectComponent } from './choice-select/choice-select.component'
 import { ChatHistoryComponent } from './chat-history/chat-history.component'
-/* eslint-disable @nx/enforce-module-boundaries */
+
 import { buildCodayEvent, CodayEvent, ErrorEvent } from '@coday/coday-events'
 import { CodayEventHandler } from './utils/coday-event-handler'
 import { HeaderComponent } from './header/header.component'
