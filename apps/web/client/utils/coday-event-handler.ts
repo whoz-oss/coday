90f37a5c5207fd74707ed67d2634b5b116478c7a
vincent.audibert <vincent.audibert@whoz.com>
2025-08-05 17:43:35 +0200

    chore: remove previous frontend

--- apps/web/client/utils/coday-event-handler.ts
+++
@@ -1,6 +0,0 @@
-
-import { CodayEvent } from '@coday/coday-events'
-
-export interface CodayEventHandler {
-  handle(event: CodayEvent): void
-}
--- apps/web/client/utils/marked.ts
+++
@@ -1,1 +0,0 @@
-declare const marked: { parse: (src: string) => string | Promise<string> }
