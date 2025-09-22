9d756441c3785a87a3b98b007f379070a6709b09
vincent.audibert <vincent.audibert@whoz.com>
2025-09-22 10:53:48 +0200

    on openai assistants too

--- libs/handler/openai.client.ts
+++ libs/handler/openai.client.ts
@@ -458,11 +458,11 @@ export class OpenaiClient extends AiClient {
                 try {
                   this.interactor.sendEvent(request)
                   responseEvent = await agent.tools.run(request)
-                  this.interactor.sendEvent(responseEvent)
                 } catch (error: any) {
                   console.error(`Error running tool ${request.name}:`, error)
                   responseEvent = request.buildResponse(`Error: ${error.message}`)
                 }
+                this.interactor.sendEvent(responseEvent)
                 thread.addToolRequests(agent.name, [request])
                 thread.addToolResponseEvents([responseEvent])
                 toolOutputs.push({
