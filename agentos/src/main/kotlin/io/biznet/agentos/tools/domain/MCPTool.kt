1fe820d087ff995f1f3fff264286ccbebdd9cd57
vincent.couturier <vincent.couturier@whoz.com>
2025-11-14 18:35:58 +0100

    feat: wz-28134 improve tool registry and start coding MCP tools provider

--- agentos/src/main/kotlin/io/biznet/agentos/tools/domain/MCPTool.kt
+++ agentos/src/main/kotlin/io/biznet/agentos/tools/domain/MCPTool.kt
@@ -2,9 +2,10 @@ package io.biznet.agentos.tools.domain
 
 data class MCPTool(override val name: String, override val version: String,
                    override val description: String,
-                   override val inputSchema: String
-) : StandardTool {
-    override fun execute(input: String): String =
+                   override val paramType: Class<*>? = String::class.java,
+                   override val inputSchema: String = NO_ARGS_INPUT
+) : StandardTool<String> {
+    override fun execute(input: String?): String =
         "result of MCP to do ```${description}``` on $input"
 
 }
--- agentos/src/main/kotlin/io/biznet/agentos/tools/domain/StandardTool.kt
+++ agentos/src/main/kotlin/io/biznet/agentos/tools/domain/StandardTool.kt
@@ -1,9 +1,18 @@
 package io.biznet.agentos.tools.domain
 
-interface StandardTool {
+interface StandardTool<T> {
     val name: String
     val description: String
     val inputSchema: String
     val version: String
-    fun execute(input: String): String
+    val paramType: Class<*>?
+    fun execute(input: T?): String
 }
+
+const val NO_ARGS_INPUT = """{
+                            "${"$"}schema" : "https://json-schema.org/draft/2020-12/schema",
+                            "type" : "object",
+                            "properties" : { "input": { "type": "null" } },
+                            "required" : [ ],
+                            "additionalProperties" : false
+                        }"""
