80b2c4b8539700b8411713c8b663fc24dcdd1e08
vincent.audibert <vincent.audibert@whoz.com>
2025-12-09 11:02:04 +0100

    feat: work on EntityService interface

--- agentos/src/main/kotlin/io/biznet/agentos/orchestration/ICaseEventService.kt
+++ agentos/src/main/kotlin/io/biznet/agentos/orchestration/ICaseEventService.kt
@@ -1,5 +1,14 @@
 package io.biznet.agentos.orchestration
 
-interface ICaseEventService {
-    fun save(event: CaseEvent): CaseEvent
-}
+import io.biznet.agentos.common.EntityService
+import java.util.UUID
+
+/**
+ * Service for managing CaseEvent entities.
+ *
+ * Extends EntityService with no additional methods.
+ * Parent type is UUID representing the caseId.
+ *
+ * Implementation must ensure that findByParent returns events ordered by timestamp (oldest first).
+ */
+interface ICaseEventService : EntityService<CaseEvent, UUID>
