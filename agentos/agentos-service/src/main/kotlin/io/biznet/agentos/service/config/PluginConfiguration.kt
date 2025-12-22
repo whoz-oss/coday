e4ef631e9881fca2e26a80db0f8cdc4ce305138b
vincent.couturier <vincent.couturier@whoz.com>
2025-12-22 09:05:59 +0100

    build(agentos): fix test issue on github CI and apply proper code format

--- agentos/agentos-service/src/main/kotlin/io/biznet/agentos/service/config/PluginConfiguration.kt
+++ agentos/agentos-service/src/main/kotlin/io/biznet/agentos/service/config/PluginConfiguration.kt
@@ -1,9 +1,11 @@
 package io.biznet.agentos.service.config
 
+import io.biznet.agentos.service.config.properties.AgentOsPluginsConfigProperties
 import org.pf4j.PluginManager
 import org.pf4j.spring.SpringPluginManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.nio.file.Files
 import java.nio.file.Paths
 
 /**
@@ -11,11 +13,16 @@ import java.nio.file.Paths
  */
 @Configuration
 class PluginConfiguration {
-    
     @Bean
-    fun pluginManager(): PluginManager {
-        val pluginsDir = System.getProperty("pf4j.pluginsDir", "plugins")
-        return SpringPluginManager(Paths.get(pluginsDir)).apply {
+    fun pluginManager(pluginsConfigProperties: AgentOsPluginsConfigProperties): PluginManager {
+        val pluginPath = Paths.get(pluginsConfigProperties.dir)
+
+        // Create plugins directory if it doesn't exist
+        if (!Files.exists(pluginPath)) {
+            Files.createDirectories(pluginPath)
+        }
+
+        return SpringPluginManager(pluginPath).apply {
             loadPlugins()
             startPlugins()
         }
