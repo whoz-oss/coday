73a44f016cef12f56083517d24ac71be02d326f7
yannick.bride <yannick.bride@whoz.com>
2026-01-08 11:15:43 +0100

    WZ-28668: correct settings.gradle.kts

--- agentos/settings.gradle.kts
+++ agentos/settings.gradle.kts
@@ -1,5 +1,13 @@
 rootProject.name = "agentos"
 
+
+// Enable version catalog
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+
+includeBuild("agentos-sdk")
+includeBuild("agentos-service")
+
 // Include API modules
 include("plugin-api")
 
