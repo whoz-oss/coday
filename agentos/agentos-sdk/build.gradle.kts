e2490eb50449988729080e5de39fb4afa38053f0
Vincent Couturier - Whoz <vincent.couturier@whoz.com>
2026-01-14 15:57:17 +0100

    feat(agentos): ensure publishing of agent-os sdk during release action (#429)

--- agentos/agentos-sdk/build.gradle.kts
+++ agentos/agentos-sdk/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("dev.nx.gradle.project-graph") version("0.1.10")
+    id("dev.nx.gradle.project-graph") version ("0.1.10")
     alias(libs.plugins.kotlin.jvm)
     `maven-publish`
 }
 
-group = "io.biznet.agentos"
+group = "whoz-oss.agentos"
 version = libs.versions.agentosSdk.get()
 description = "AgentOS SDK - Plugin interfaces and extension points"
 
@@ -22,10 +22,10 @@ dependencies {
         exclude(group = "org.slf4j", module = "slf4j-reload4j")
         exclude(group = "org.slf4j", module = "slf4j-log4j12")
     }
-    
+
     // Kotlin standard library
     implementation(libs.kotlin.stdlib)
-    
+
     // Test dependencies
     testImplementation(libs.bundles.testing.common)
     testRuntimeOnly(libs.junit.platform.launcher)
@@ -41,19 +41,19 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
-            
+
             pom {
                 name.set("AgentOS SDK")
                 description.set("Plugin interfaces and extension points for developing AgentOS plugins")
                 url.set("https://github.com/whoz-oss/coday")
-                
+
                 licenses {
                     license {
                         name.set("Apache License 2.0")
                         url.set("https://www.apache.org/licenses/LICENSE-2.0")
                     }
                 }
-                
+
                 developers {
                     developer {
                         id.set("whoz-oss")
@@ -61,7 +61,7 @@ publishing {
                         email.set("oss@whoz.com")
                     }
                 }
-                
+
                 scm {
                     connection.set("scm:git:git://github.com/whoz-oss/coday.git")
                     developerConnection.set("scm:git:ssh://github.com/whoz-oss/coday.git")
@@ -70,10 +70,10 @@ publishing {
             }
         }
     }
-    
+
     repositories {
         mavenLocal()
-        
+
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/whoz-oss/coday")
