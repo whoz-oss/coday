00da948271e4f53be963a53de1f4f53aa87e25fe
Vincent Palita <vincent.palita@whoz.com>
2026-01-27 00:03:14 +0100

    feat: almost no circular deps
    Signed-off-by: Vincent Palita <vincent.palita@whoz.com>

--- libs/model/src/lib/coday-options.ts
+++ libs/model/src/lib/coday-options.ts
@@ -1,7 +1,7 @@
-import * as path from 'path'
-import * as os from 'os'
+import path from 'node:path'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import os from 'node:os'
 
 // Define the argv type
 export interface Argv {
