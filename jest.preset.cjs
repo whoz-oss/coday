2ebd217e33356879741048c3be18a8bd35bf8320
Vincent Palita <vincent.palita@whoz.com>
2026-01-28 23:51:55 +0100

    feat: fix test for client
    Signed-off-by: Vincent Palita <vincent.palita@whoz.com>

--- jest.preset.cjs
+++ jest.preset.cjs
@@ -10,16 +10,6 @@ module.exports = {
   coverageDirectory: `${process.env.NX_WORKSPACE_ROOT}/coverage/${process.env.NX_TASK_TARGET_PROJECT}`,
   coverageReporters: ['html'],
 
-  // CORRECTION : Utiliser <rootDir>/../../ pour remonter au workspace root
-  moduleNameMapper: {
-    '^@coday/model',
-    '^@coday/ai-thread/(.*)$': '<rootDir>/../../libs/ai-thread/$1',
-    '^@coday/core$': '<rootDir>/../../libs/coday.ts',
-    '^@coday/model',
-    '^@coday/model',
-    '^@coday/service/(.*)$': '<rootDir>/../../libs/service/$1',
-  },
-
   // Exclude problematic directories
   modulePathIgnorePatterns: [
     '<rootDir>/dist/',
