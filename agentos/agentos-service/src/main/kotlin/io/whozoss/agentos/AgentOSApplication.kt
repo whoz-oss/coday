88cddbe6bc4b7ba625f016158bb58399410d25ac
thomas.martin <thomas.martin@whoz.com>
2026-02-05 10:05:26 +0100

    feat: wz-28668: fixes after merge

--- agentos/agentos-service/src/main/kotlin/io/whozoss/agentos/AgentOSApplication.kt
+++ agentos/agentos-service/src/main/kotlin/io/whozoss/agentos/AgentOSApplication.kt
@@ -1,12 +1,7 @@
-<<<<<<<< HEAD:agentos/agentos-service/src/main/kotlin/io/whozoss/agentos/AgentOSApplication.kt
 package io.whozoss.agentos
 
 import io.whozoss.agentos.config.properties.AgentOsPluginsConfigProperties
-========
-package io.whozoss.agentos.service
-
 import io.whozoss.agentos.service.config.AgentOsPluginsConfigProperties
->>>>>>>> origin/master:agentos/agentos-service/src/main/kotlin/io/whozoss/agentos/service/AgentOSApplication.kt
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
