8edc1b115dfb2d1949e571c23a120050a773c453
vincent.audibert <vincent.audibert@whoz.com>
2026-02-26 11:51:15 +0100

    feat: #481 add smoke test status on hello-agentos

--- libs/agentos-ui/src/lib/components/hello-agentos/hello-agentos.component.ts
+++ libs/agentos-ui/src/lib/components/hello-agentos/hello-agentos.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core'
+import { Component, inject, OnInit, signal } from '@angular/core'
+import { PluginControllerService } from '@whoz-oss/agentos-api-client'
 import { IconButtonComponent } from '@whoz-oss/design-system'
 
 /**
@@ -16,8 +17,18 @@ import { IconButtonComponent } from '@whoz-oss/design-system'
   templateUrl: './hello-agentos.component.html',
   styleUrl: './hello-agentos.component.scss',
 })
-export class HelloAgentosComponent {
+export class HelloAgentosComponent implements OnInit {
+  private readonly pluginService = inject(PluginControllerService)
+
   protected lastAction = signal<string | null>(null)
+  protected agentosOnline = signal<boolean | null>(null)
+
+  ngOnInit(): void {
+    this.pluginService.getAllPlugins().subscribe({
+      next: () => this.agentosOnline.set(true),
+      error: () => this.agentosOnline.set(false),
+    })
+  }
 
   protected onSend(): void {
     this.lastAction.set('send')
