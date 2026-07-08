fbc450aaeeb66ded938959d6382a7b630593915e
vincent.audibert <vincent.audibert@whoz.com>
2026-07-08 10:30:08 +0200

    feat: regen openapi and client

--- libs/agentos-api-client/src/lib/model/namespace-list-item.ts
+++ libs/agentos-api-client/src/lib/model/namespace-list-item.ts
@@ -12,20 +12,11 @@ export interface NamespaceListItem {
   id: string
   name: string
   description?: string
-  /**
-   * Optional filesystem path to a directory containing base configuration for this namespace
-   */
   configPath?: string
-  /**
-   * Optional external identifier for this namespace, e.g. a federation id from an external system
-   */
   externalId?: string
-  /**
-   * Logical name of the default agent for this namespace. Null means no default is configured. Name matching is case-insensitive.
-   */
   defaultAgentName?: string
   /**
-   * The caller\'s effective role on this namespace
+   * The caller\'s effective role on this namespace.
    */
   role: NamespaceListItemRoleEnum
 }
