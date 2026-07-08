659d7fb18405c8134f9cec0ad12d93eee4da652b
vincent.audibert <vincent.audibert@whoz.com>
2026-07-08 11:15:13 +0200

    feat: regen openapi and client

--- libs/agentos-api-client/src/lib/api/prompt-controller.service.ts
+++ libs/agentos-api-client/src/lib/api/prompt-controller.service.ts
@@ -407,7 +407,7 @@ export class PromptControllerService extends BaseService {
   }
 
   /**
-   * @param parentId Parent entity ID
+   * @param parentId
    * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
    * @param reportProgress flag to report request and response progress.
    */
