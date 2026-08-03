00833b747162a278027b3b5f95905da6f2d38701
vincent.audibert <vincent.audibert@whoz.com>
2026-08-03 15:32:30 +0200

    fix: adapt FilesystemPromptRepository and test to post-merge API changes

--- agentos/agentos-service/src/test/kotlin/io/whozoss/agentos/prompt/PromptControllerExportSpec.kt
+++ agentos/agentos-service/src/test/kotlin/io/whozoss/agentos/prompt/PromptControllerExportSpec.kt
@@ -15,6 +15,7 @@ import io.mockk.every
 import io.mockk.mockk
 import io.whozoss.agentos.exception.ResourceNotFoundException
 import io.whozoss.agentos.namespace.NamespaceService
+import io.whozoss.agentos.permissions.OverlayScopeAuthorizer
 import io.whozoss.agentos.permissions.PermissionService
 import io.whozoss.agentos.sdk.entity.EntityMetadata
 import io.whozoss.agentos.user.UserService
@@ -35,7 +36,8 @@ class PromptControllerExportSpec : StringSpec({
     val namespaceService = mockk<NamespaceService>(relaxed = true)
     val userService = mockk<UserService>(relaxed = true)
     val permissionService = mockk<PermissionService>(relaxed = true)
-    val controller = PromptController(promptService, namespaceService, userService, permissionService)
+    val overlayScopeAuthorizer = mockk<OverlayScopeAuthorizer>(relaxed = true)
+    val controller = PromptController(promptService, namespaceService, userService, permissionService, overlayScopeAuthorizer)
 
     val namespaceId = UUID.randomUUID()
 
